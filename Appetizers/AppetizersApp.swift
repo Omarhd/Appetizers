6d9665de2d5df8d32dba592e04f17bc8735ec8dd
Omar <omeralhadu@gmail.com>
2022-01-18 18:43:30 +0200

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Omar Abdulrahman on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
